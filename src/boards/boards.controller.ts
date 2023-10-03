feabf6c3b7ba939d4a9287cfa8c2606daa0068f5
moong2 <www.castlehi@gmail.com>
2023-10-03 22:56:12 +0900

    feat: add getBoardById using TypeORM

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,13 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Board } from './board.entity';
 
 @Controller('boards')
 export class BoardsController {
+	constructor(private boardsService: BoardsService) {}
+
+	@Get('/:id')
+	getBoardById(@Param('id') id: number): Promise <Board> {
+		return this.boardsService.getBoardById(id);
+	}
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,22 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BoardRepository } from './board.repository';
+import { Board } from './board.entity';
 
 @Injectable()
 export class BoardsService {
+	constructor (
+		@InjectRepository(BoardRepository)
+		private boardRepository: BoardRepository,
+	){}
+
+	async getBoardById(id: number): Promise <Board> {
+		const found = await this.boardRepository.findOneBy({id});
+
+		if (!found) {
+			throw new NotFoundException(`Can't find Board with id ${id}`);
+		}
+		
+		return found;
+	}
 }
