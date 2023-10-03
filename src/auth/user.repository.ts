4aec44c5a98a1d337415791d646efb4afdf67092
moong2 <www.castlehi@gmail.com>
2023-10-04 00:34:18 +0900

    feat: encrypt password

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,6 +2,7 @@ import { DataSource, Repository } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredentialDto } from "./dto/auth-credential.dto";
 import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -11,7 +12,11 @@ export class UserRepository extends Repository<User> {
 
 	async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
 		const { username, password } = authCredentialDto;
-		const user = this.create({ username, password });
+
+		const salt = await bcrypt.genSalt();
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+		const user = this.create({ username, password: hashedPassword });
 
 		try {
 			await this.save(user);
