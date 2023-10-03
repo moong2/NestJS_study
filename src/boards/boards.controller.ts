e9c395860bad363c5185ae2438713a71ec4c3250
moong2 <www.castlehi@gmail.com>
2023-10-03 22:36:50 +0900

    fix: remove code to change postgreSQL

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,42 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
-import { Board, BoardStatus } from './boards.model';
-import { BoardsService } from './boards.service';
-import { CreateBoardDto } from './dto/create-board.dto';
-import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
-	constructor(private boardsService: BoardsService) {}
-
-	@Get('/')
-	getAllBoard(): Board[] {
-		return this.boardsService.getAllBoards();
-	}
-
-	@Post()
-	@UsePipes(ValidationPipe)
-	createBoard(
-		@Body() createBoardDto: CreateBoardDto
-	): Board {
-		return this.boardsService.createBoard(createBoardDto);
-	}
-
-	@Get('/:id')
-	getBoardById(@Param('id') id: string): Board{
-		//param이 여러 개라면 @Param()으로 처리
-		return this.boardsService.getBoardById(id);
-	}
-
-	@Delete('/:id')
-	deleteBoard(@Param('id') id: string): void {
-		this.boardsService.deleteBoard(id);
-	}
-
-	@Patch('/:id/status')
-	updateBoardStatus(
-		@Param('id') id: string,
-		@Body('status', BoardStatusValidationPipe) status: BoardStatus
-	): Board {
-		return this.boardsService.updateBoardStatus(id, status);
-	}
 }
