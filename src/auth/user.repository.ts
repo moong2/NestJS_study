8cd32d4775e0b9a1689567e31320c39bdeb3b92b
moong2 <www.castlehi@gmail.com>
2023-10-04 00:28:30 +0900

    feat: make username unique

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,7 @@
 import { DataSource, Repository } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredentialDto } from "./dto/auth-credential.dto";
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -13,6 +13,11 @@ export class UserRepository extends Repository<User> {
 		const { username, password } = authCredentialDto;
 		const user = this.create({ username, password });
 
-		await this.save(user);
+		try {
+			await this.save(user);
+		} catch (error) {
+			if (error.code === '23505') throw new ConflictException(`Existing username`);
+			else throw new InternalServerErrorException();
+		}
 	}
 }
