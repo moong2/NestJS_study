ddcb217458bed7447f9005f5b6f7687668db176b
moong2 <www.castlehi@gmail.com>
2023-10-05 14:40:28 +0900

    feat: get all boards that made by specific user

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -21,8 +21,10 @@ export class BoardsController {
 	}
 
 	@Get()
-	getAllBoard(): Promise<Board[]> {
-		return this.boardsService.getAllBoards();
+	getAllBoard(
+		@GetUser() user: User
+	): Promise<Board[]> {
+		return this.boardsService.getAllBoards(user);
 	}
 
 	@Get('/:id')
